d4fec4df54311bf747a70d33d934c8adb96bb530
Daiana Silva <daiana.silva@govbr.com.br>
2022-10-19 21:01:25 -0300

    Criação projeto bd persistencia

---
+++ persistencia/emprestimo/src/main/java/br/edu/infnet/emprestimo/App.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.emprestimo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ persistencia/emprestimo/src/main/java/br/edu/infnet/emprestimo/modelo/entidades/Emprestimo.java
@@ -0,0 +1,5 @@
+package br.edu.infnet.emprestimo.modelo.entidades;
+
+public class Emprestimo {
+
+}
